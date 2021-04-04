30cdfeadf9224603ae15c00398dc996c5de0cbc8
Ayush Mall <ayushmall@Ayushs-MacBook-Pro.local>
2021-04-04 23:00:07 +0530

    APP UI Added

---
+++ app/src/main/java/com/ayushmall/notes/Login_Register.java
@@ -0,0 +1,18 @@
+package com.ayushmall.notes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class Login_Register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login__register);
+    }
+
+    public void handleLoginRegister(View view) {
+    }
+}
